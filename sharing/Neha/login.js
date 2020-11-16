f18c8dc443fd8310d37772ccb82acf2e6ff7d520
Nikhil Abraham <nikhilabraham@Nikhils-MacBook-Air.local>
2020-11-16 23:21:06 +0530

    initial JS validation completed

--- sharing/Neha/login.js
+++ sharing/Neha/login.js
@@ -1,34 +1,48 @@
+const form = document.getElementById("form");
+
+console.log("JS RUNNING");
+
+
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   validate();
+// })
+
+
 function validate(){
-  var email=document.getElementById("email").value;
-  var password=document.getElementById("password").value;
+  var email=document.getElementById("email").value.trim();
+  var password=document.getElementById("password").value.trim();
   var email_error=document.getElementById("email_error");
   var password_error=document.getElementById("password_error");
+  var flag=0;
+  console.log("Flag3");
 
   if(email.length<9){
-    email.style.border="1px solid red";
     email_error.style.display="block";
-    email.focus();
-    alert("invalid email");
+    console.log("Email-Error!");
+    flag = 0;
   }
   else{
-    email.style.border="1px solid grey";
     email_error.style.display="none";
-    email.focus();
-
+    console.log("Email-Correct");
+    flag = 1;
   }
-  if(password.value.length <6){
-    password.style.border="1px solid red";
+  if(password.length <6){
     password_error.style.display="block";
-    password.focus();
-    alert("invalid password");
-    
+    console.log("Pass-Error!")
+    flag = 0;
   }
-  else
-  {
-  password.style.border="1px solid grey";
-  password_error.style.display="none";
-  password.focus();
+  else{
+    password_error.style.display="none";
+    console.log("Pass-Correct!");
+    flag = 1;
+  }
 
-  
+  if(flag){
+    return true;
+  } 
+  else{
+    return false;
   }
 }
