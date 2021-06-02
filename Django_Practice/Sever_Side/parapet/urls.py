979bf9c3982bea2cd722f91da5fe43082b539fff
Nikhil Abraham <nikhilabraham@Nikhils-MacBook-Air.local>
2021-06-02 21:52:47 +0530

    linked login and signup page

--- Django_Practice/Sever_Side/parapet/urls.py
+++ Django_Practice/Sever_Side/parapet/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path("",views.index, name="index"),
   path("login/",views.login, name="login"),
-  path("signup/",views.signup, name="signup"),
+  path("signup/",views.signup, name="signup")
 ]
