dddd2a6f32c0b918f64625a7fa844f73b93b8c29
Nikhil Abraham <nikhilabraham@Nikhils-MacBook-Air.local>
2021-06-04 21:04:08 +0530

    added sample changes to server side

--- Django_Practice/Sever_Side/Sever_Side/urls.py
+++ Django_Practice/Sever_Side/Sever_Side/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('parapet/', include("parapet.urls")),
+    path('accounts/', include("accounts.urls")),
 ]
